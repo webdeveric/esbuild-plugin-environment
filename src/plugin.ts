50bee3b870e186cdb33c776d6d11182e7c1eb727
Eric King <eric@webdeveric.com>
2024-12-26 19:04:14 -0600

    chore: updated types

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,22 +1,18 @@
-import type { Plugin } from 'esbuild';
-
 import { definePlugin } from 'esbuild-plugin-define';
+
+import type { Plugin } from 'esbuild';
 
 export const PLUGIN_NAME = 'environment-plugin';
 
-type StringRecord = Record<string, string>;
-
-type StringTuple = [key: string, value: string];
-
-export const environmentPlugin = (data: string[] | StringRecord): Plugin => ({
+export const environmentPlugin = (
+  data: string[] | Record<string, string | number | boolean | bigint | undefined | null>,
+): Plugin => ({
   name: PLUGIN_NAME,
   async setup(build) {
-    const entries = Array.isArray(data) ? data.map<StringTuple>(key => [key, '']) : Object.entries(data);
+    const entries = Array.isArray(data) ? data.map((key) => [key, '']) : Object.entries(data);
 
-    const env: StringRecord = Object.fromEntries(
-      entries.map(([key, defaultValue]) => {
-        return [key, String(process.env[key] ?? defaultValue)];
-      }),
+    const env: Record<string, string> = Object.fromEntries(
+      entries.map(([key, defaultValue]) => [key, String(process.env[key] ?? defaultValue)]),
     );
 
     await definePlugin({ process: { env } }).setup(build);
